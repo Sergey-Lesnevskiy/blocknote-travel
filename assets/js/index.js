1afd04b83f356de7c17c522e86ea371945f8a420
Lesnevsky-Sergey <78030028+Sergey-Lesnevskiy@users.noreply.github.com>
2025-02-23 18:21:11 +0300

    feat: added validation on first page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,8 @@
-const data ={
-  dataTravels: [
-  {
-    title:'Несвиж',
-    timeStart: '01.01.01',
-    timeEnd: '01.01.01',
-    dataNotes:[
-      {
-        img: '../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png', allImage:['../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png','../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png','../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png'
-        ], date: '2025-02-05', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores doloribus, omnis illo quos rerum tenetur sint odio quaerat aspernatur consequuntur ratione. Necessitatibus optio dolor illo, harum sit inventore possimus cumque.'
-      },
-      {
-        img: '../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png', allImage:['../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png','../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png','../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png'
-        ], date: '2025-02-05', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores doloribus, omnis illo quos rerum tenetur sint odio quaerat aspernatur consequuntur ratione. Necessitatibus optio dolor illo, harum sit inventore possimus cumque.'
-      },
-      {
-        img: '../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png', allImage:['../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png','../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png','../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png'
-        ], date: '2025-02-05', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores doloribus, omnis illo quos rerum tenetur sint odio quaerat aspernatur consequuntur ratione. Necessitatibus optio dolor illo, harum sit inventore possimus cumque.'
-      },
-    ]
-  },
-  {
-    title:'Мир',
-    timeStart: '01.01.01',
-    timeEnd: '01.01.01',
-    dataNotes:[
-      {
-        img: '../assets/img/image (3)-fotor-bg-remover-20250215153649 (1).png', allImage:['../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png','../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png','../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png'
-        ], date: '2025-02-05', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores doloribus, omnis illo quos rerum tenetur sint odio quaerat aspernatur consequuntur ratione. Necessitatibus optio dolor illo, harum sit inventore possimus cumque.'
-      },
-      {
-        img: '../assets/img/image (3)-fotor-bg-remover-20250215153649 (1).png', allImage:['../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png','../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png','../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png'
-        ], date: '2025-02-05', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores doloribus, omnis illo quos rerum tenetur sint odio quaerat aspernatur consequuntur ratione. Necessitatibus optio dolor illo, harum sit inventore possimus cumque.'
-      },
-      {
-        img: '../assets/img/image (3)-fotor-bg-remover-20250215153649 (1).png', allImage:['../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png','../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png','../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png'
-        ], date: '2025-02-05', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores doloribus, omnis illo quos rerum tenetur sint odio quaerat aspernatur consequuntur ratione. Necessitatibus optio dolor illo, harum sit inventore possimus cumque.'
-      },
-    ]
-  }
-],
-keyTravel:null,
-keyNote:null,
+let data = {
+
 }
+// const main = document.querySelector('main');
+
 const popup = document.querySelector('.popup');
 const popupContainer = document.querySelector('.popup_container');
 const closePopup = document.querySelector('.close_popup');
@@ -51,6 +12,39 @@ const body = document.querySelector('body');
 // const dataFile = document.querySelector('.input_file');
 const containerFieldsNote = document.querySelector('.containerFieldsNote');
 const addedNote = document.querySelector('.added-note');
+
+
+const titlePopup = popup.querySelector(`.input_title`);
+const startTimePopup = popup.querySelector(`.input_start`);
+const endTimePopup = popup.querySelector(`.input_end`);
+
+let isValid = true;
+
+function isSessionEmpty() {
+  let dataSession = JSON.parse(localStorage.getItem("dataBlocknote"));
+  if (dataSession) {
+    data = dataSession;
+    console.log(data);
+  } else {
+    console.log('tam');
+    data = {
+      dataTravels: [
+        {
+          title: null,
+          timeStart: null,
+          timeEnd: null,
+          dataNotes: [
+          ]
+        },
+      ],
+      keyTravel: null,
+      keyNote: null,
+    };
+  }
+}
+function saveSession() {
+  localStorage.setItem('dataBlocknote', JSON.stringify(data));
+}
 
 function createTravel(data, index) {
   let element = document.createElement('li');
@@ -68,15 +62,33 @@ function createTravel(data, index) {
   list.append(element);
 }
 function createListTravel(data) {
-  data.forEach((element, i) => {
-    createTravel(element, i)
-  });
-}
-createListTravel(data.dataTravels);
+  if (data.dataTravels[0].title) {
+    data.dataTravels.forEach((element, i) => {
+      createTravel(element, i)
+    });
 
-list.addEventListener('click',(e)=>{
+  } else {
+    let element = document.createElement('li');
+    element.classList.add('emptyList');
+    element.innerHTML = `<h2>
+  Cоздайте поездку первую поездку;
+  </h2>
+  `;
+    list.prepend(element);
+  }
+}
+isSessionEmpty();
+createListTravel(data);
+
+function rerenderingListTravel(data) {
+  list.innerHTML = '';
+  createListTravel(data);
+}
+
+list.addEventListener('click', (e) => {
   data.keyTravel = e.target.dataset.item
-  localStorage.setItem('dataBlocknote', JSON.stringify(data))
+  // localStorage.setItem('dataBlocknote', JSON.stringify(data))
+  saveSession();
   window.location.href = "/pages/traveling.html";
 })
 
@@ -86,17 +98,17 @@ newTravel.addEventListener('click', () => {
   openPopup();
 })
 
-function openPopup(openPopupInterface, number){
-  if(openPopupInterface === 'correct'){
+function openPopup(openPopupInterface, number) {
+  if (openPopupInterface === 'correct') {
     setTimeout(() => {
       popupContainer.classList.toggle('show');
       dateNote.value = data[number].date;
-      textNote.value =  data[number].description;
+      textNote.value = data[number].description;
       // imgNote.value =  data[number].img;
     }, 200)
     popup.classList.toggle('show');
     body.classList.toggle('lock')
-  }else{
+  } else {
     setTimeout(() => {
       popupContainer.classList.toggle('show');
     }, 200)
@@ -111,7 +123,9 @@ function closePopupButton() {
     popupContainer.classList.toggle('show');
   }, 200)
   body.classList.toggle('lock');
-  defaultValues();
+  // defaultValues();
+  removeClassValid();
+  saveSession();
 }
 
 closePopup.addEventListener('click', closePopupButton);
@@ -126,7 +140,7 @@ closePopup.addEventListener('click', closePopupButton);
 //   }
 // });
 let noteNum = 1
-function createfieldsNote(){
+function createFieldsNote() {
   noteNum++;
   let element = document.createElement('li');
   element.classList.add('fieldsNote');
@@ -145,52 +159,93 @@ function createfieldsNote(){
           <textarea id="text" name="text" class="new-note-description-${noteNum}" rows="5" cols="33"></textarea>
         </label>
         `
-        containerFieldsNote.append(element);
+  containerFieldsNote.append(element);
 }
-addedNote.addEventListener('click',()=>{
-  createfieldsNote();
+addedNote.addEventListener('click', () => {
+  createFieldsNote();
 })
 
+function validNote(){
+  for (let i = 1; i <= noteNum; i++) {
+    const arrayFields = [
+    popup.querySelector(`.input_file-${i}`),
+    popup.querySelector(`.input_date-${i}`),
+    popup.querySelector(`.new-note-description-${i}`),
+    ];
+
+    arrayFields.forEach(element => {
+      if (!isValidationPopup(element)) {
+        console.log('here');
+        isValid = isValidationPopup(element);
+        addClassValid(arrayFields);
+        return
+      }
+    });
+  }
+}
+
+
+function addedDataNote() {
+  const array = []
+  for (let i = 1; i <= noteNum; i++) {
+    const arrayFields = [popup.querySelector(`.input_file-${i}`),
+    popup.querySelector(`.input_date-${i}`),
+    popup.querySelector(`.new-note-description-${i}`),
+    ];
+
+    arrayFields.forEach(element => {
+      if (isValidationPopup(element)) {
+        isValid = isValidationPopup(element);
+        addClassValid(arrayFields);
+        return;
+      }
+    });
+    if (isValid) {
+      const data = {
+        img: popup.querySelector(`.input_file-${i}`).value,
+        date: popup.querySelector(`.input_date-${i}`).value,
+        description: popup.querySelector(`.new-note-description-${i}`).value,
+      }
+      array.push(data);
+    } else {
+      return;
+    }
+  }
+  return array;
+}
 
 saveNote.addEventListener('click', (e) => {
-  // console.log(dataFile.value);
 
-  // console.log(popup.querySelector('.containerFieldsNote'));
-  const data = {
-    img: popup.querySelector(`.input_file-${noteNum}`).value,
-    date: popup.querySelector(`.input_date-${noteNum}`).value,
-    description: popup.querySelector(`.new-note-description-${noteNum}`).value,
-  }   
-  console.log(data);
-//    <label>Выберите фото:
-//   <input type="file" class="input_file-1">
-// </label>
-// <label>
-//   Выберите дату:
-//   <input type="date" class="input_date-1">
-// </label>
-// <label class="textarea-label label">Введите текст заметки:
-//   <textarea id="text" name="text" class="new-note-description-1" rows="5" cols="33"></textarea>
-// </label>
-  // {
-  //   title:'Несвиж',
-  //   timeStart: '01.01.01',
-  //   timeEnd: '01.01.01',
-  //   dataNotes:[
-  //     {
-  //       img: '../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png', allImage:['../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png','../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png','../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png'
-  //       ], date: '2025-02-05', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores doloribus, omnis illo quos rerum tenetur sint odio quaerat aspernatur consequuntur ratione. Necessitatibus optio dolor illo, harum sit inventore possimus cumque.'
-  //     },
-  //     {
-  //       img: '../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png', allImage:['../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png','../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png','../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png'
-  //       ], date: '2025-02-05', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores doloribus, omnis illo quos rerum tenetur sint odio quaerat aspernatur consequuntur ratione. Necessitatibus optio dolor illo, harum sit inventore possimus cumque.'
-  //     },
-  //     {
-  //       img: '../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png', allImage:['../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png','../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png','../assets/img/image (2)-fotor-bg-remover-20250215153238 (1).png'
-  //       ], date: '2025-02-05', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores doloribus, omnis illo quos rerum tenetur sint odio quaerat aspernatur consequuntur ratione. Necessitatibus optio dolor illo, harum sit inventore possimus cumque.'
-  //     },
-  //   ]
-  // },
+  const arrayInputs = [
+    titlePopup, startTimePopup, endTimePopup
+  ]
+
+  if (isValidationPopup(titlePopup) && isValidationPopup(startTimePopup) && isValidationPopup(endTimePopup)&& validNote()) {
+    let newTravel = {
+      title: popup.querySelector(`.input_title`).value,
+      timeStart: popup.querySelector(`.input_start`).value,
+      timeEnd: popup.querySelector(`.input_end`).value,
+      dataNotes: addedDataNote()
+    }
+    data.dataTravels.push(newTravel);
+    rerenderingListTravel(data);
+    removeClassValid();
+    closePopupButton();
+  } else {
+    validNote();
+    addClassValid(arrayInputs);
+  }
+  //    <label>Выберите фото:
+  //   <input type="file" class="input_file-1">
+  // </label>
+  // <label>
+  //   Выберите дату:
+  //   <input type="date" class="input_date-1">
+  // </label>
+  // <label class="textarea-label label">Введите текст заметки:
+  //   <textarea id="text" name="text" class="new-note-description-1" rows="5" cols="33"></textarea>
+  // </label>
+
 
 
   // if(openPopupInterface === 'correct'){
@@ -212,7 +267,7 @@ saveNote.addEventListener('click', (e) => {
 // read file
 // async function readImageFile(file) {
 //   const reader = new FileReader();
-  
+
 //   return new Promise((resolve, reject) => {
 //       reader.onload = event => resolve(event.target.result); /* Мы нашли решение. */
 //       reader.onerror = reject;
@@ -231,3 +286,26 @@ saveNote.addEventListener('click', (e) => {
 // }
 // read file
 //popup
+
+// validation
+function isValidationPopup(element) {
+  if (!element.value.trim()) {
+    return false;
+  }
+  return true;
+}
+function addClassValid(array) {
+  array.forEach((el) => {
+    isValidationPopup(el) ?
+      el.classList.remove('no-valid') :
+      el.classList.add('no-valid')
+  })
+}
+function removeClassValid() {
+  popup.querySelectorAll('.no-valid').forEach(el=>{el.classList.remove('no-valid')})
+  // array.forEach((el) => {
+  //   el.classList.remove('no-valid');
+  // })
+}
+
+// validation
